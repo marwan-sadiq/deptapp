87e929de4e3b3ba1c0f86ef0549563ff950dccae
marwan-sadiq <mervan.sezek@gmail.com>
2025-10-11 00:13:25 +0300

    Update API URL to PythonAnywhere backend and fix test mocks

--- Desktop/marketapp/frontend/src/api.ts
+++ Desktop/marketapp/frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://donnmero.pythonanywhere.com/api/',
 })
 
 export type Customer = {
